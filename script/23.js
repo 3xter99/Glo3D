2ef0ac8c610e1378f1cc71488c62221b6f00943f
3xter <3x-ter@mail.ru>
2020-09-13 13:17:50 +0300

    lesson25

--- script/23.js
+++
@@ -1,3 +0,0 @@
-let task1 = document.getElementById('task1')
-
-task1.innerHTML = task1.innerHTML.replace(/функц[а-я]*/g, '<strong>$&</strong>')
--- script/script.js
+++ script/script.js
@@ -383,6 +383,78 @@ const tabs = () => {
     }
     calc(100)
 
+    const mainForm = new Validator({
+        selector: '#form1',
+        pattern: {
+            phone: /^\+380\d{9}$/
+        },
+        method: {
+            'form1-phone': [
+                ['notEmpty'],
+                ['pattern', 'phone']
+            ],
+            'form1-email': [
+                ['notEmpty'],
+                ['pattern', 'email']
+            ],
+            'form1-name': [
+                ['notEmpty'],
+                ['pattern', 'name']
+            ]
+        }
+    });
+
+    mainForm.init();
+
+    const footerForm = new Validator({
+        selector: '#form2',
+        pattern: {
+            phone: /^\+380\d{9}$/
+        },
+        method: {
+            'form2-phone': [
+                ['notEmpty'],
+                ['pattern', 'phone']
+            ],
+            'form2-email': [
+                ['notEmpty'],
+                ['pattern', 'email']
+            ],
+            'form2-name': [
+                ['notEmpty'],
+                ['pattern', 'name']
+            ],
+            'form2-message': [
+                ['notEmpty'],
+                ['pattern', 'message']
+            ]
+        }
+    });
+
+    footerForm.init();
+
+    const popupForm = new Validator({
+        selector: '#form3',
+        pattern: {
+            phone: /^\+380\d{9}$/
+        },
+        method: {
+            'form3-phone': [
+                ['notEmpty'],
+                ['pattern', 'phone']
+            ],
+            'form3-email': [
+                ['notEmpty'],
+                ['pattern', 'email']
+            ],
+            'form3-name': [
+                ['notEmpty'],
+                ['pattern', 'name']
+            ]
+        }
+    });
+
+    popupForm.init();
 
 
 
